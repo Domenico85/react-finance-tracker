0de158043294c97d371cdfd1f320272524b53b19
Dome <arillotta@gmail.com>
2025-02-19 15:20:31 +0100

    added dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import { Card } from "react-bootstrap";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2 className="mb-4">Dashboard</h2>
+      <Card className="p-4">
+        <h4>Overview</h4>
+        <p>Track your finances and manage expenses easily.</p>
+      </Card>
+    </div>
+  );
+};
+
+export default Dashboard;
