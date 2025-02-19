745ccb323048029aa32df6d19225f42d1fd5eb7d
Dome <arillotta@gmail.com>
2025-02-19 15:41:33 +0100

    added context

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
 import Navbar from "./components/Navbar";
+import { TransactionProvider } from "./context/TransactionContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className="center-container">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/transactions" element={<Transactions />} />
-          </Routes>
+    <TransactionProvider>
+      <Router>
+        <Navbar />
+        <div className="center-container">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/transactions" element={<Transactions />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </TransactionProvider>
   );
 }
 
---
+++ src/context/TransactionContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useContext } from "react";
+
+const TransactionContext = createContext();
+
+export const useTransactions = () => {
+  return useContext(TransactionContext);
+};
+
+export const TransactionProvider = ({ children }) => {
+  const [transactions, setTransactions] = useState([]);
+
+  // Add a new transaction
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]);
+  };
+
+  return (
+    <TransactionContext.Provider value={{ transactions, addTransaction }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+};
